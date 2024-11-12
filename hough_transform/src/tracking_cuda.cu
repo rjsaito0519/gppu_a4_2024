3150f804c727b800626653e3a1bc06bfcd136509
rjsaito0519 <ryuta.saito.q3@dc.tohoku.ac.jp>
2024-11-12 13:56:34 +0900

    Tue, Nov 12, 2024  1:56:33 PM

--- hough_transform/src/tracking_cuda.cu
+++ hough_transform/src/tracking_cuda.cu
@@ -145,7 +145,7 @@ std::vector<std::vector<int>> tracking_cuda(const std::vector<TVector3>& pos_con
         int _max_rho   = max_index % n_rho - static_cast<int>((n_rho-1)/2);
         auto end_time6 = std::chrono::high_resolution_clock::now();
         auto duration6 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time6 - start_time6).count();
-        std::cout << "max_element2: " << duration5 << " ns" << std::endl;
+        std::cout << "max_element2: " << duration6 << " ns" << std::endl;
         std::cout << "max_index, " << max_index << std::endl;
 
 
